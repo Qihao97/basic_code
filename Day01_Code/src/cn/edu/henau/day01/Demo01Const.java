6425db8ddeaf612340a36a4000bbabeb0dc3a4f1
Qihao97 <qihao_97@126.com>
2019-11-06 21:17:36 +0800

    学习Java时的练习小程序

--- Day01_Code/src/cn/edu/henau/day01/Demo01Const.java
+++ Day01_Code/src/cn/edu/henau/day01/Demo01Const.java
@@ -4,6 +4,6 @@ public class Demo01Const {
     public static void main(String[] args) {
         String s = "NanYang, XinYe";
         System.out.println(s);
-
+        System.out.println(123456);
     }
 }
