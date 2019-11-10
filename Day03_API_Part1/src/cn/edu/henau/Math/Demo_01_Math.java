364d3cc6298581d206b17df901c05cb6a198d6a1
Qihao97 <qihao_97@126.com>
2019-11-10 08:44:54 +0800

    学习Java时的练习小程序

---
+++ Day03_API_Part1/src/cn/edu/henau/Math/Demo_01_Math.java
@@ -0,0 +1,23 @@
+package cn.edu.henau.Math;
+
+/*
+* java.util.Math类是数学相关的工具类，里面提供了大量的静态方法，完成与数学相关的操作
+*
+* 1.求绝对值：
+*   public static double abs(double num)
+* 2.向上取整：
+*   public static double ceil(double num)
+* 3.向下取整:
+*   public static double floor(double num)
+* 4.四舍五入：
+*   public static long round(double num)
+* */
+public class Demo_01_Math {
+    public static void main(String[] args) {
+        double d = 3.1415926;
+        System.out.println(Math.abs(d));
+        System.out.println(Math.ceil(d));
+        System.out.println(Math.floor(d));
+        System.out.println(Math.round(d));
+    }
+}
