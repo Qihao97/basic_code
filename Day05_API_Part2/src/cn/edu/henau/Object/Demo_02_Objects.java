e9f31dcd26f140915c43312ec1190fb4c108fbdc
Qihao97 <qihao_97@126.com>
2019-11-11 14:34:16 +0800

    学习Java时的练习小程序

--- Day05_API_Part2/src/cn/edu/henau/Object/Demo_02_Objects.java
+++ Day05_API_Part2/src/cn/edu/henau/Object/Demo_02_Objects.java
@@ -1,7 +1,19 @@
 package cn.edu.henau.Object;
 
+import java.util.Objects;
+
 /*
 * Objects类的equals方法：对两个对象进行比较，防止空指针异常
+* public static boolean equals(Object a,Object b){
+*     return (a == b || (a != null && a.equals(b));
+* }
 * */
 public class Demo_02_Objects {
+    public static void main(String[] args) {
+        String s1 = null;
+        String s2 = "abc";
+
+        boolean b2 = Objects.equals(s1,s2);
+        System.out.println(b2);
+    }
 }
