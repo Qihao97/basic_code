aca3aa666f565d04bc9b9e8fbe63a1e58c28b620
Qihao97 <qihao_97@126.com>
2019-11-21 21:52:47 +0800

    测试代码

--- day06_collection/src/cn/edu/henau/generic/generic_wildcard/GenericWildcardAdvance.java
+++ day06_collection/src/cn/edu/henau/generic/generic_wildcard/GenericWildcardAdvance.java
@@ -4,6 +4,8 @@ package cn.edu.henau.generic.generic_wildcard;
  * Author: Qihao
  * Date: 2019/11/21  21:48
  * Content:
+ * 泛型的上限限定: ? extends E 代表使用的泛型只能是E类型的子类/本身
+ * 泛型的下限限定: ? super E   代表使用的泛型只能是E类型的父类/本身
  */
 public class GenericWildcardAdvance {
 }
