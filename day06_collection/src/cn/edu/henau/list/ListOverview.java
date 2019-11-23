14846a7bfb932b3ef7bfe396a72e22dc44dabbcc
Qihao97 <qihao_97@126.com>
2019-11-23 16:35:43 +0800

    测试代码

---
+++ day06_collection/src/cn/edu/henau/list/ListOverview.java
@@ -0,0 +1,15 @@
+package cn.edu.henau.list;
+
+/**
+ * Author: Qihao
+ * Date: 2019/11/22  20:01
+ * Content:
+ * List集合介绍和常用的方法
+ * java.util.List接口 extends Collection接口
+ * List接口的特点：
+ *    1.有序的集合，存储元素和取出元素的顺序是一致的（存储123，取出123）
+ *    2.有索引,包含了一些带索引的方法
+ *    3.
+ */
+public class ListOverview {
+}
