6d4a3d60131edea2ec26dac2b6fdb1cc24239e2b
Qihao97 <qihao_97@126.com>
2019-11-23 20:39:40 +0800

    测试代码

---
+++ day06_collection/src/cn/edu/henau/collectionSet/HashSetOverview.java
@@ -0,0 +1,10 @@
+package cn.edu.henau.collectionSet;
+
+/**
+ * Author: Qihao
+ * Date: 2019/11/23  20:39
+ * Content:
+ *
+ */
+public class HashSetOverview {
+}
---
+++ day06_collection/src/cn/edu/henau/vector/DemoVector.java
@@ -0,0 +1,11 @@
+package cn.edu.henau.vector;
+
+/**
+ * Author: Qihao
+ * Date: 2019/11/23  20:32
+ * Content:
+ * Vector类可以实现可增长的对象数组
+ * Vector是同步的,单线程,效率低,已淘汰
+ */
+public class DemoVector {
+}
