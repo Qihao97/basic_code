c7dd32aeab52e60b49a63aa0dd7c54647ce979f5
Qihao97 <qihao_97@126.com>
2019-11-25 21:36:09 +0800

    测试代码

--- day06_collection/src/cn/edu/henau/map_collection/DemoEntry.java
+++ day06_collection/src/cn/edu/henau/map_collection/DemoEntry.java
@@ -12,6 +12,16 @@ import java.util.Map;
  *
  * Map.Entry<K,V>: 在Map接口中有一个内部接口Entry
  * 作用: 当Map集合一创建,就会在Map集合中创建一个Entry对象,用来记录键与值(键值对 对象,键与值的映射关系)
+ *
+ * Map集合遍历的第二种方法,使用Entry对象遍历
+ *
+ * Map集合中的方法, Set<Map.Entry<k,v>> entrySet(): 返回此映射中包含的映射关系的 Set 视图
+ *
+ * 实现步骤:
+ *    1.使用 Map集合中的方法entrySet(0,把 Map集合中多个 Entry对象取出来,存储到一个Set集合中
+ *    2.遍历Set集合,获取每一个Entry对象
+ *    3.使用Entry对象中的方法getKey()和getValue()方法获取键与值
+ *
  */
 public class DemoEntry {
     public static void main(String[] args) {
